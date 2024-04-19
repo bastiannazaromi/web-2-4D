0d0343c20f93619f05d32178503b12c20c0ed2aa
Bastian Nazaromi <bastian.nazaromi@gmail.com>
2024-04-19 08:28:52 +0700

    fix pertemuan 7

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -18,6 +18,36 @@ class Admin extends CI_Controller
 
 		$this->load->view('index', $data);
 	}
+
+	public function a()
+	{
+		function sumOddNumbers($N, $M)
+		{
+			// Pastikan N adalah bilangan ganjil
+			if ($N % 2 == 0) {
+				$N += 1;
+			}
+
+			// Jika N lebih besar dari M, tidak ada bilangan ganjil lagi untuk dijumlahkan
+			if ($N > $M) {
+				return 0;
+			}
+
+			echo $N . ',';
+			// Jumlahkan N dengan hasil rekursif dari N + 2 dan M
+			return $N + sumOddNumbers($N + 1, $M);
+		}
+
+		// Contoh penggunaan fungsi
+		$N1 = 9;
+		$M1 = 9;
+		echo "Hasil jumlah bilangan ganjil antara $N1 dan $M1: " . sumOddNumbers($N1, $M1) . "\n"; // Output: 54 (5 + 7 + 9 + 11 + 13 + 15)
+
+		$N2 = 10;
+		$M2 = 13;
+		echo "Hasil jumlah bilangan ganjil antara $N2 dan $M2: " . sumOddNumbers($N2, $M2) . "\n"; // Output: 96 (11 + 13 + 15 + 17 + 19 + 21)
+
+	}
 }
 
 /* End of file Admin.php */
--- application/views/index.php
+++ application/views/index.php
@@ -10,22 +10,19 @@
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 	<!-- Font Awesome -->
 	<link rel="stylesheet" href="<?php echo base_url('assets/'); ?>plugins/fontawesome-free/css/all.min.css">
-	<!-- Ionicons -->
-	<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-	<!-- Tempusdominus Bootstrap 4 -->
-	<link rel="stylesheet" href="<?php echo base_url('assets/'); ?>plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-	<!-- iCheck -->
-	<link rel="stylesheet" href="<?php echo base_url('assets/'); ?>plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-	<!-- JQVMap -->
-	<link rel="stylesheet" href="<?php echo base_url('assets/'); ?>plugins/jqvmap/jqvmap.min.css">
 	<!-- Theme style -->
 	<link rel="stylesheet" href="<?php echo base_url('assets/'); ?>dist/css/adminlte.min.css">
 	<!-- overlayScrollbars -->
 	<link rel="stylesheet" href="<?php echo base_url('assets/'); ?>plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-	<!-- Daterange picker -->
-	<link rel="stylesheet" href="<?php echo base_url('assets/'); ?>plugins/daterangepicker/daterangepicker.css">
-	<!-- summernote -->
-	<link rel="stylesheet" href="<?php echo base_url('assets/'); ?>plugins/summernote/summernote-bs4.min.css">
+
+	<!-- Datatables -->
+	<link rel="stylesheet" href="<?= base_url('assets/'); ?>plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+	<link rel="stylesheet" href="<?= base_url('assets/'); ?>plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+
+	<!-- jQuery -->
+	<script src="<?php echo base_url('assets/'); ?>plugins/jquery/jquery.min.js"></script>
+	<!-- jQuery UI 1.11.4 -->
+	<script src="<?php echo base_url('assets/'); ?>plugins/jquery-ui/jquery-ui.min.js"></script>
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
@@ -109,25 +106,31 @@
 							</a>
 						</li>
 						<li class="nav-item">
-							<a href="<?php echo base_url('admin/user'); ?>" class="nav-link">
+							<a href="<?php echo base_url('user'); ?>" class="nav-link">
 								<i class="nav-icon fas fa-user"></i>
 								<p>User</p>
 							</a>
 						</li>
 						<li class="nav-item">
-							<a href="<?php echo base_url('admin/film'); ?>" class="nav-link">
-								<i class="nav-icon fas fa-video"></i>
-								<p>Film</p>
-							</a>
-						</li>
-						<li class="nav-item">
-							<a href="<?php echo base_url('admin/cinema'); ?>" class="nav-link">
+							<a href="<?php echo base_url('cinema'); ?>" class="nav-link">
 								<i class="nav-icon fas fa-person-booth"></i>
 								<p>Cinema</p>
 							</a>
 						</li>
 						<li class="nav-item">
-							<a href="<?php echo base_url('admin/orders'); ?>" class="nav-link">
+							<a href="<?php echo base_url('film'); ?>" class="nav-link">
+								<i class="nav-icon fas fa-video"></i>
+								<p>Film</p>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="<?php echo base_url('jadwal'); ?>" class="nav-link">
+								<i class="nav-icon fas fa-calendar"></i>
+								<p>Jadwal Tayang</p>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="<?php echo base_url('orders'); ?>" class="nav-link">
 								<i class="nav-icon fas fa-wallet"></i>
 								<p>Orders</p>
 							</a>
@@ -156,40 +159,33 @@
 	</div>
 	<!-- ./wrapper -->
 
-	<!-- jQuery -->
-	<script src="<?php echo base_url('assets/'); ?>plugins/jquery/jquery.min.js"></script>
-	<!-- jQuery UI 1.11.4 -->
-	<script src="<?php echo base_url('assets/'); ?>plugins/jquery-ui/jquery-ui.min.js"></script>
-	<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-	<script>
-		$.widget.bridge('uibutton', $.ui.button)
-	</script>
 	<!-- Bootstrap 4 -->
 	<script src="<?php echo base_url('assets/'); ?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 	<!-- ChartJS -->
-	<script src="<?php echo base_url('assets/'); ?>plugins/chart.js/Chart.min.js"></script>
-	<!-- Sparkline -->
-	<script src="<?php echo base_url('assets/'); ?>plugins/sparklines/sparkline.js"></script>
-	<!-- JQVMap -->
-	<script src="<?php echo base_url('assets/'); ?>plugins/jqvmap/jquery.vmap.min.js"></script>
-	<script src="<?php echo base_url('assets/'); ?>plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
-	<!-- jQuery Knob Chart -->
-	<script src="<?php echo base_url('assets/'); ?>plugins/jquery-knob/jquery.knob.min.js"></script>
-	<!-- daterangepicker -->
-	<script src="<?php echo base_url('assets/'); ?>plugins/moment/moment.min.js"></script>
-	<script src="<?php echo base_url('assets/'); ?>plugins/daterangepicker/daterangepicker.js"></script>
-	<!-- Tempusdominus Bootstrap 4 -->
-	<script src="<?php echo base_url('assets/'); ?>plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
-	<!-- Summernote -->
-	<script src="<?php echo base_url('assets/'); ?>plugins/summernote/summernote-bs4.min.js"></script>
 	<!-- overlayScrollbars -->
 	<script src="<?php echo base_url('assets/'); ?>plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 	<!-- AdminLTE App -->
 	<script src="<?php echo base_url('assets/'); ?>dist/js/adminlte.js"></script>
-	<!-- AdminLTE for demo purposes -->
-	<script src="<?php echo base_url('assets/'); ?>dist/js/demo.js"></script>
-	<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-	<script src="<?php echo base_url('assets/'); ?>dist/js/pages/dashboard.js"></script>
+
+	<!-- DataTables  & Plugins -->
+	<script src="<?= base_url('assets/'); ?>plugins/datatables/jquery.dataTables.min.js"></script>
+	<script src="<?= base_url('assets/'); ?>plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+	<script src="<?= base_url('assets/'); ?>plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+	<script src="<?= base_url('assets/'); ?>plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+
+	<script>
+		$(function() {
+			$('#example1').DataTable({
+				"paging": true,
+				"lengthChange": true,
+				"searching": true,
+				"ordering": true,
+				"info": true,
+				"autoWidth": true,
+				"responsive": true,
+			});
+		});
+	</script>
 </body>
 
 </html>
