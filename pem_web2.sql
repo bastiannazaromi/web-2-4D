4576c081889b0a8020c6ccb2895bbea0cddc1c20
Bastian Nazaromi <bastian.nazaromi@gmail.com>
2024-04-26 08:09:50 +0700

    update database

--- pem_web2.sql
+++ pem_web2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 27, 2024 at 07:09 AM
+-- Generation Time: Apr 26, 2024 at 03:06 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.33
 
@@ -24,16 +24,87 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `cinema`
+--
+
+CREATE TABLE `cinema` (
+  `id` int(2) NOT NULL,
+  `namaCinema` varchar(30) NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cinema`
+--
+
+INSERT INTO `cinema` (`id`, `namaCinema`, `createdAt`, `updatedAt`) VALUES
+(1, 'Cinema 1', '2024-04-19 02:45:53', NULL),
+(3, 'Cinema 2', '2024-04-19 03:12:52', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `film`
+--
+
+CREATE TABLE `film` (
+  `id` int(11) NOT NULL,
+  `judul` varchar(50) NOT NULL,
+  `genre` varchar(25) NOT NULL,
+  `durasi` int(3) NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jadwal_tayang`
+--
+
+CREATE TABLE `jadwal_tayang` (
+  `id` int(11) NOT NULL,
+  `idCinema` int(2) NOT NULL,
+  `idFilm` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `jamTayang` time NOT NULL,
+  `jumlahKursi` int(3) NOT NULL,
+  `kursiTerjual` int(3) NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `idUser` int(11) NOT NULL,
+  `idJadwal` int(11) NOT NULL,
+  `jumlah` int(3) NOT NULL,
+  `no_kursi` int(3) NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `password` varchar(255) NOT NULL,
   `nama` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `tahun` int(4) NOT NULL,
+  `no_hp` varchar(14) NOT NULL,
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
   `updatedAt` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -42,13 +113,36 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`, `nama`, `email`, `tahun`, `createdAt`, `updatedAt`) VALUES
-(3, 'admin', 'jslalf', 'Admin', 'admin@gmail.com', 2024, '2024-03-13 07:13:19', '2024-03-22 02:49:10'),
-(32, 'lhhl', '$2y$10$Kr8I1dOrRz2nf3L6ZGwjbuVCQzShX/36M2t5hcT8fvGYWhwTUI78O', 'lkhlkh', 'lkhlk@gmail.com', 1234, '2024-03-22 03:01:30', NULL);
+INSERT INTO `user` (`id`, `nama`, `email`, `username`, `password`, `tahun`, `no_hp`, `createdAt`, `updatedAt`) VALUES
+(1, 'Admin', 'admin@gmail.com', 'admin', '$2y$10$.pUHewzPqCdcnaOt5sUNf.FU6CTsEo.Rjki7jVeU7Bwb/cu74sN5K', 2024, '', '2024-03-13 07:13:19', '2024-04-19 01:34:44');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `cinema`
+--
+ALTER TABLE `cinema`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `film`
+--
+ALTER TABLE `film`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jadwal_tayang`
+--
+ALTER TABLE `jadwal_tayang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
@@ -61,10 +155,34 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `cinema`
+--
+ALTER TABLE `cinema`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `film`
+--
+ALTER TABLE `film`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `jadwal_tayang`
+--
+ALTER TABLE `jadwal_tayang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
